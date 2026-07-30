77edbb762502726e914f26e0d52f30ac3987badd
raebaexxx <vadimfeda@yandex.ru>
2026-07-30 23:25:07 +0300

    adapt to fuxi + cleanup some sepolicy rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/nuwa-miuicamera/BoardConfigVendor.mk
+include vendor/xiaomi/fuxi-miuicamera/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/nuwa-miuicamera
+MIUICAMERA_PATH := device/xiaomi/fuxi-miuicamera
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from the proprietary version
-$(call inherit-product, vendor/xiaomi/nuwa-miuicamera/nuwa-miuicamera-vendor.mk)
+$(call inherit-product, vendor/xiaomi/fuxi-miuicamera/fuxi-miuicamera-vendor.mk)
 
 # Public libraries
 PRODUCT_COPY_FILES += \
